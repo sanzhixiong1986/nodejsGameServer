0e7c7fd8132f6d7e457e07f8f1e7dbbe24aa34bb
sanzhixiong1986 <57082213+sanzhixiong1986@users.noreply.github.com>
2022-03-08 23:56:09 +0400

    对服务器的模块进行分解

--- GameServer/test/talk_room.js
+++ GameServer/test/talk_room.js
@@ -1,11 +1,12 @@
 var log = require("../uitl/log.js");
 var proto_man = require("../netbus/proto_man.js");
 
-
+const STYPE_TALKROOM = 1;//作为主服务器的id号，1号服务器是聊天
+const STYPE_NAMES = "talk room"
 
 var service = {
-	stype: 1, // 服务号
-	name: "talk room", // 服务名称
+	stype: STYPE_TALKROOM, // 服务号
+	name: STYPE_NAMES, // 服务名称
 
 	// 每个服务初始化的时候调用
 	init: function () {
@@ -15,7 +16,7 @@ var service = {
 	// 每个服务收到数据的时候调用
 	on_recv_player_cmd: function(session, ctype, body) {
 		log.info(this.name + " on_recv_player_cmd: ", ctype, body);
-        	
+        //收到消息进行判断
         var cmd_buf = proto_man.encode_cmd(proto_man.PROTO_JSON, 1, 2, "Hello Talk room from ws!!!");
 	    session.send(cmd_buf);	
 	},
